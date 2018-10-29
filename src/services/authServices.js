9f9ac5f488aefaef90174f257ef4cfc2477082d0
Lei Qin <lei.qin@symbio.com>
2018-10-29 16:27:08 +0800

    去除statte 操作

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -16,7 +16,7 @@ class AuthService {
   }
 
   // 获取用户信息
-  GetUserInfo({ commit, state }) {
+  GetUserInfo() {
     return new Promise((resolve, reject) => {
       getUserInfo(state.token).then(response => {
         if (!response.data) { // 由于mockjs 不支持自定义状态码只能这样hack
@@ -25,14 +25,9 @@ class AuthService {
         const data = response.data
 
         if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
-          commit('SET_ROLES', data.roles)
         } else {
           reject('getInfo: roles must be a non-null array !')
         }
-
-        commit('SET_NAME', data.name)
-        commit('SET_AVATAR', data.avatar)
-        commit('SET_INTRODUCTION', data.introduction)
         resolve(response)
       }).catch(error => {
         reject(error)
